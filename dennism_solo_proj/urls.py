6b706acbcfbcd99ba63215215ef2ffdde8627fa1
Dennis Muljadi <dennis.mulyadi@gmail.com>
2020-07-22 04:51:32 -0700

    edit proj.urls and admin capabilities

--- dennism_solo_proj/urls.py
+++ dennism_solo_proj/urls.py
@@ -4,10 +4,27 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from login_register_app.models import User
+from trip_app.models import Trip, Itinerary, Expense, PhotoAlbum
 
 class UserAdmin(admin.ModelAdmin):
     pass
-admin.site.register(User,UserAdmin);
+admin.site.register(User,UserAdmin)
+
+class TripAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Trip,TripAdmin)
+
+class ItineraryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Itinerary,ItineraryAdmin)
+
+class ExpenseAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Expense,ExpenseAdmin)
+
+class PhotoAlbumAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PhotoAlbum,PhotoAlbumAdmin)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
